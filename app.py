61677165e1ef81c8f9a0f0c20e103b8dfe0cb18b
Jason <jasoncmcvicar@gmail.com>
2022-10-07 15:40:31 -0500

    fleshing out app.py so we can seed our db

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
 """Flask app for Cupcakes"""
+
+from flask import Flask, redirect, render_template, request, flash
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import db, connect_db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "nope"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakz"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+connect_db(app)
+db.create_all()
+
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
